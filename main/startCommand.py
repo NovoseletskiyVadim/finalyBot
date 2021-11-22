bfd59f0760a4a590ef149230ef027468bb72bdf2
Безсонов Руслан <ttm121212@gmail.com>
2021-11-22 18:11:23 +0200

    Add start command Hundler

---
+++ main/startCommand.py
@@ -0,0 +1,21 @@
+
+
+def start(bot, my_keyBoard):
+
+    #обработчик команды "старт"
+    @bot.message_handler(commands=['start'])
+    def welcome(message):
+
+        sti=open('sticker/AnimatedSticker1.tgs', 'rb')
+        bot.send_sticker(message.chat.id,sti)
+
+  
+        bot.send_message(
+            message.chat.id,
+            "Добро пожаловать,"+
+            " {0.first_name}!\nЯ - <b>\"{1.first_name}\"</b>,".format(message.from_user, bot.get_me())+
+            " бот созданный чтобы помогать бороться с наркотиками. \n Жду информацию!",
+            parse_mode='html',
+            reply_markup=my_keyBoard[0]
+           )
+
