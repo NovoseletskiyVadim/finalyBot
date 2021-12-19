c5014984d5f074366d81aee913f9fb27b230c59b
Vadim <novoseletskiy.v@gmail.com>
2021-12-19 23:28:32 +0200

    Revert "Create function "checkUserUpdate ""
    This reverts commit ca420c014ef7cf1b854f215aa887d72fb3d6c743.

--- main/logs/addNewUser.py
+++ main/logs/addNewUser.py
@@ -3,40 +3,10 @@ checkUser=classes.checkNewUser
 
 #check_status==True or False
 
-#словарь для checkUserUpdate
-checkUpdateDictuonart={}
-
 def addUser(message,check_status):
 
 
     user=checkUser(message.chat.id,message.from_user )
 
     user.addSaveNewUser(check_status)
-
-
-def checkUserUpdate():
-
-    #обращение и считиывние slogStartCommand
-    #считывание данных  в словарь если словарь пуст 
-    #проверка команды старт .. если она есть в файле то не писать старт в файл= в startCommand
-
-    #TODO:сделать проверку словаря 
-
-    #если словарь пуст загрузить его из файла
-    
-    
-    #если команла старт есть в словаре то разрешать чат.если нет то требрвать ввода команлы старт 
-
-    #при вводе писать старт и в словарь и в фвйл
-
-    #словарь перенести в main
-
-    if checkUserUpdate:
-
-        print("dictionary exists")
-
-    else:
-
-        print("dictionary does not exist")
-
 
