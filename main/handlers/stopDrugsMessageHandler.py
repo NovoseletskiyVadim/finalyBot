7204ba953bd52be037df4f179583e6867c44f930
Безсонов Руслан <ttm121212@gmail.com>
2021-12-20 18:19:51 +0200

    Add button in checkUpdate

--- main/handlers/stopDrugsMessageHandler.py
+++ main/handlers/stopDrugsMessageHandler.py
@@ -24,7 +24,7 @@ def drugsMessageHundler(bot, my_keyBoard, array, commandStartDictuonary):
     @bot.message_handler(content_types=["text", "photo", "audio", "document","location" ])
     def handle_text(message):
         
-        checkUpdate=addNewUser.checkUserUpdate(bot, message,commandStartDictuonary)
+        checkUpdate=addNewUser.checkUserUpdate(bot, message,commandStartDictuonary, my_keyBoard)
        
         if message.chat.type == "private" and checkUpdate:
            
